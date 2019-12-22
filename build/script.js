65710ceb69a65c130de1c34df5ff2066529c9d0e
maksim <skarobogyodko@gmail.com>
2019-12-22 18:20:27 +0300

    change js

--- build/script.js
+++ build/script.js
@@ -1,36 +1,37 @@
-const switchTheme = e => {
-  e.target.parentElement.classList.toggle("onoffswitch_checked");
-  
-  const defaultTheme = document.querySelectorAll(".theme_color_project-default");
-  const inverseTheme = document.querySelectorAll(".theme_color_project-inverse")
+if (typeof window !== 'undefined') {
 
-  for(elem of defaultTheme) {
-    elem.classList.remove("theme_color_project-default");
-    elem.classList.add("theme_color_project-inverse");
-  }
-  for(elem of inverseTheme) {
-    elem.classList.remove("theme_color_project-inverse");
-    elem.classList.add("theme_color_project-default");
-  }
-};
+  const switchTheme = e => {
+    e.target.parentElement.classList.toggle("onoffswitch_checked");
 
-const history = e => {
-  e.target.nextElementSibling.classList.toggle("history__hide");
-};
+    const defaultTheme = document.querySelectorAll(".theme_color_project-default");
+    const inverseTheme = document.querySelectorAll(".theme_color_project-inverse")
 
-if(typeof window !== undefined) {
-  
-document.body.addEventListener("click", (e) => {
-  const elem = e.target
+    for (elem of defaultTheme) {
+      elem.classList.remove("theme_color_project-default");
+      elem.classList.add("theme_color_project-inverse");
+    }
+    for (elem of inverseTheme) {
+      elem.classList.remove("theme_color_project-inverse");
+      elem.classList.add("theme_color_project-default");
+    }
+  };
 
-  if(elem.classList.contains("onoffswitch__button")) {
-    switchTheme(e);
-    return;
-  }
+  const history = e => {
+    e.target.nextElementSibling.classList.toggle("history__hide");
+  };
 
-  if (elem.classList.contains("history__show")) {
-    history(e);
-  } 
 
-});
+  document.body.addEventListener("click", (e) => {
+    const elem = e.target
+
+    if (elem.classList.contains("onoffswitch__button")) {
+      switchTheme(e);
+      return;
+    }
+
+    if (elem.classList.contains("history__show")) {
+      history(e);
+    }
+
+  });
 }
